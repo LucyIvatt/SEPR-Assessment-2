a638158154d1c0f5f273c39a2b5955b11e7386c4
BrunoDavies <bd775@york.ac.uk>
2019-11-18 13:01:24 +0000

    Added JUnit for Entity - and all additional files

---
+++ core/src/com/mygdx/game/sprites/EntityTest.java
@@ -0,0 +1,26 @@
+package com.mygdx.game.sprites;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntityTest {
+
+    @org.junit.jupiter.api.Test
+    void getPosition() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWidth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHeight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTexture() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTexture() {
+    }
+}

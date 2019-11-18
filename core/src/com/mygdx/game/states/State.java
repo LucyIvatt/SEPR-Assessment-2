74a5eb8ba46fd7ce2c632f6142231249c867af23
Lucy Ivatt <li571@york.ac.uk>
2019-11-18 20:43:49 +0000

    added movement to entity
    - should be for firetruck but just messing around to make sure it works

--- core/src/com/mygdx/game/states/State.java
+++ core/src/com/mygdx/game/states/State.java
@@ -3,6 +3,8 @@ package com.mygdx.game.states;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public abstract class State {
 

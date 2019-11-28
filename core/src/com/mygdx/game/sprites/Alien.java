a1078823cf3dbdd9fc49a87f0fd7fd2c1ab8107f
GeckoGamez <alasdairpilmorebedford@gmail.com>
2019-11-28 13:00:06 +0000

    Update Alien.java

--- core/src/com/mygdx/game/sprites/Alien.java
+++ core/src/com/mygdx/game/sprites/Alien.java
@@ -24,39 +24,12 @@ public class Alien extends Character {
         moveTo();
         seeTarget();
     }
-    // hit boxes are arrays with length 4, where each vector3 represents the corners of the hitbox
-    private Vector3[] createHitBox(){
-        Vector3[] hitBox = new Vector3[4];
-        Vector3 currentSquare = getPosition();
-        int range = getRange();
-        //TL
-        hitBox[0].x = currentSquare.x - range;
-        hitBox[0].y = currentSquare.y + range;
-        //TR
-        hitBox[1].x = currentSquare.x + range;
-        hitBox[1].y = currentSquare.y + range;
-        //BL
-        hitBox[2].x = currentSquare.x - range;
-        hitBox[2].y = currentSquare.y - range;
-        //BR
-        hitBox[3].x = currentSquare.x + range;
-        hitBox[3].y = currentSquare.y - range;
-        return hitBox;
-    }
 
     // gets an array of all fireTrucks in game
     private Firetruck[] getAllFireTrucks(){
         Firetruck[] alltrucks = new Firetruck[4];
         // must access a method in game state manager or something?
         return alltrucks;
-    }
-
-    // checks if a fireTruck is in the search area can be extended to check if walls are in front of firetrucks
-    private boolean isInArea(Vector3 fireTruck, Vector3[] location){
-        if (fireTruck.x <= location[1].x && fireTruck.x >= location[0].x && fireTruck.y <= location[0].y && fireTruck.y >= location[2].y){
-            return true;
-        } else
-            return false;
     }
 
     // checks for fireTrucks in sight area and sets as target if found

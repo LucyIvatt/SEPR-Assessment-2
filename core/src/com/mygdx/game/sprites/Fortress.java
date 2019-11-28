1a0242ff49d37c3759d8ec569867b89348657aa3
matthew-lapis <matthew.gilmore18@gmail.com>
2019-11-28 14:44:06 +0000

    commented out minigame classes, fixed issues with alien and fortress

--- core/src/com/mygdx/game/sprites/Fortress.java
+++ core/src/com/mygdx/game/sprites/Fortress.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.math.Vector2;
 import java.util.Timer;
 import java.util.TimerTask;
 import com.badlogic.gdx.math.Vector3;
+import com.mygdx.game.Test;
+
 import java.util.Random;
 
 public class Fortress extends Unit {
@@ -44,37 +46,37 @@ public class Fortress extends Unit {
         this.spawnRate = new_spawnRate;
     }
 
-    public void produceAlien(int spawnRate) {
-        while (aliens.length() < 20) {
-            timer.schedule(new TimerTask(){
-                @Override
-                public void run() {
+//    public void produceAlien(int spawnRate) {
+//        while (aliens.size() < 20) {
+//            timer.schedule(new TimerTask(){
+//                @Override
+//                public void run() {
+//
+//                    while (true) {
+//                        Random rand = new Random();
+//                        int x1 = rand.nextInt(Test.WIDTH);
+//                        int y1 = rand.nextInt(Test.HEIGHT);
+//                        int x2 = rand.nextInt(Test.WIDTH);
+//                        int y2 = rand.nextInt(Test.HEIGHT);
+//
+//                        Vector2 waypoint1 = new Vector2(x1,y1);
+//                        Vector2 waypoint2 = new Vector2(x2,y2);
+//                        if (checkWaypoint(waypoint1) && checkWaypoint(waypoint2)) {
+//                            break;
+//                        }
+//                    }
+//
+//                    //Vector2[] waypoints = {waypoint1, waypoint2};
+//                    //Alien alien = new Alien(, 10, 10, 100, 10, );
+//                    //aliens.add(alien);
+//                }
+//            },1000*this.spawnRate,1000*this.spawnRate); // 1000*5=5000 mlsec. i.e. 5 seconds. u can change accordngly
+//            // given two times, first is for first time to execute this
+//            // code and second is for interval time
+//        }
+//    }
 
-                    while (true) {
-                        Random rand = new Random();
-                        int x1 = rand.nextInt(TestEntity.getWidth());
-                        int y1 = rand.nextInt(TestEntity.getHeight());
-                        int x2 = rand.nextInt(TestEntity.getWidth());
-                        int y2 = rand.nextInt(TestEntity.getHeight());
-
-                        Vector2 waypoint1 = new Vector2(x1,y1);
-                        Vector2 waypoint2 = new Vector2(x2,y2);
-                        if (checkWaypoint(waypoint1) && checkWaypoint(waypoint2)) {
-                            break
-                        }
-                    }
-
-                    Vector2[] waypoints = [waypoint1, waypoint2];
-                    Alien alien = new Alien(waypoints, null, 10, 10, 0)
-                    aliens.add(alien);
-                }
-            },1000*this.spawnRate,1000*this.spawnRate); // 1000*5=5000 mlsec. i.e. 5 seconds. u can change accordngly
-            // given two times, first is for first time to execute this
-            // code and second is for interval time
-        }
-    }
-
-    public void checkWaypoint(Vector2 waypoint1) {
+    public boolean checkWaypoint(Vector2 waypoint1) {
          if (objectPositions.contains(waypoint1)) {
              return false;
          }
--- core/src/com/mygdx/game/states/MinigameState.java
+++ core/src/com/mygdx/game/states/MinigameState.java
@@ -1,89 +1,89 @@
-package com.mygdx.game.states;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
-import com.mygdx.game.Button;
-import com.mygdx.game.Test;
-import com.mygdx.game.sprites.Entity;
-import com.mygdx.game.sprites.Pipe;
-import com.mygdx.game.sprites.TestEntity;
-
-public class MinigameState extends State{
-    private Texture background;
-    private Pipe[] pipes;
-    private Button checkCorrect;
-    private Vector3 touchPos;
-
-    public MinigameState(GameStateManager gsm) {
-        super(gsm);
-        background = new Texture("brickwall.png");
-        Texture ob = new Texture("teal.jpg");
-        checkCorrect = new Button(new Texture("blue.jpg"), new Texture("green.jpg"), 100, 20, new Vector2(800, 200));
-        for (int i=0; i<3; i++){
-            pipes[i] = new Pipe(20, (200*i)+100, 150, 150, ob, new int[]{1, 3}, 2, new int[]{1});
-        }
-        for (int i=0; i<3; i++){
-            pipes[i] = new Pipe(200, (200*i)+100, 150, 150, ob, new int[]{1, 3}, 2, new int[]{1});
-        }
-        for (int i=0; i<3; i++){
-            pipes[i] = new Pipe(380, (200*i)+100, 150, 150, ob, new int[]{1, 3}, 2, new int[]{1});
-        }
-    }
-
-    @Override
-    public void update(float dt) {
-        handleInput();
-    }
-
-    @Override
-    public void handleInput() {
-        if (Gdx.input.justTouched()) {
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            int ordinal = 0;
-            if ((touchPos.y > 100) && (touchPos.y < 250)) {
-                if ((touchPos.x > 20) && (touchPos.x < 170)) {
-                    pipes[0].rotateOnClick();
-                } else if ((touchPos.x > 200) && (touchPos.x < 350)) {
-                    pipes[1].rotateOnClick();
-                } else if ((touchPos.x > 380) && (touchPos.x < 530)) {
-                    pipes[2].rotateOnClick();
-                }
-            } else if ((touchPos.y > 300) && (touchPos.y < 450)) {
-                if ((touchPos.x > 20) && (touchPos.x < 170)) {
-                    pipes[3].rotateOnClick();
-                } else if ((touchPos.x > 200) && (touchPos.x < 350)) {
-                    pipes[4].rotateOnClick();
-                } else if ((touchPos.x > 380) && (touchPos.x < 530)) {
-                    pipes[5].rotateOnClick();
-                }
-            } else if ((touchPos.y > 500) && (touchPos.y < 650)) {
-                if ((touchPos.x > 20) && (touchPos.x < 170)) {
-                    pipes[6].rotateOnClick();
-                } else if ((touchPos.x > 200) && (touchPos.x < 350)) {
-                    pipes[7].rotateOnClick();
-                } else if ((touchPos.x > 380) && (touchPos.x < 530)) {
-                    pipes[8].rotateOnClick();
-                }
-            }
-        }
-    }
-
-    @Override
-    public void render(SpriteBatch sb) {
-        sb.begin();
-        sb.draw(background, 0, 0, Test.WIDTH, Test.HEIGHT);
-        for (Pipe pipe : pipes){
-            sb.draw(pipe.getTexture(), pipe.getPosition().x, pipe.getPosition().y, pipe.getWidth(), pipe.getHeight());
-        }
-        sb.draw(checkCorrect.getTexture(), 800, 200, 100, 20);
-        sb.end();
-    }
-
-    @Override
-    public void dispose() {
-
-    }
-}
+//package com.mygdx.game.states;
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.Input;
+//import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.math.Vector2;
+//import com.badlogic.gdx.math.Vector3;
+//import com.mygdx.game.Button;
+//import com.mygdx.game.Test;
+//import com.mygdx.game.sprites.Entity;
+//import com.mygdx.game.sprites.Pipe;
+//import com.mygdx.game.sprites.TestEntity;
+//
+//public class MinigameState extends State{
+//    private Texture background;
+//    private Pipe[] pipes;
+//    private Button checkCorrect;
+//    private Vector3 touchPos;
+//
+//    public MinigameState(GameStateManager gsm) {
+//        super(gsm);
+//        background = new Texture("brickwall.png");
+//        Texture ob = new Texture("teal.jpg");
+//        checkCorrect = new Button(new Texture("blue.jpg"), new Texture("green.jpg"), 100, 20, new Vector2(800, 200));
+//        for (int i=0; i<3; i++){
+//            pipes[i] = new Pipe(20, (200*i)+100, 150, 150, ob, new int[]{1, 3}, 2, new int[]{1});
+//        }
+//        for (int i=0; i<3; i++){
+//            pipes[i] = new Pipe(200, (200*i)+100, 150, 150, ob, new int[]{1, 3}, 2, new int[]{1});
+//        }
+//        for (int i=0; i<3; i++){
+//            pipes[i] = new Pipe(380, (200*i)+100, 150, 150, ob, new int[]{1, 3}, 2, new int[]{1});
+//        }
+//    }
+//
+//    @Override
+//    public void update(float dt) {
+//        handleInput();
+//    }
+//
+//    @Override
+//    public void handleInput() {
+//        if (Gdx.input.justTouched()) {
+//            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+//            int ordinal = 0;
+//            if ((touchPos.y > 100) && (touchPos.y < 250)) {
+//                if ((touchPos.x > 20) && (touchPos.x < 170)) {
+//                    pipes[0].rotateOnClick();
+//                } else if ((touchPos.x > 200) && (touchPos.x < 350)) {
+//                    pipes[1].rotateOnClick();
+//                } else if ((touchPos.x > 380) && (touchPos.x < 530)) {
+//                    pipes[2].rotateOnClick();
+//                }
+//            } else if ((touchPos.y > 300) && (touchPos.y < 450)) {
+//                if ((touchPos.x > 20) && (touchPos.x < 170)) {
+//                    pipes[3].rotateOnClick();
+//                } else if ((touchPos.x > 200) && (touchPos.x < 350)) {
+//                    pipes[4].rotateOnClick();
+//                } else if ((touchPos.x > 380) && (touchPos.x < 530)) {
+//                    pipes[5].rotateOnClick();
+//                }
+//            } else if ((touchPos.y > 500) && (touchPos.y < 650)) {
+//                if ((touchPos.x > 20) && (touchPos.x < 170)) {
+//                    pipes[6].rotateOnClick();
+//                } else if ((touchPos.x > 200) && (touchPos.x < 350)) {
+//                    pipes[7].rotateOnClick();
+//                } else if ((touchPos.x > 380) && (touchPos.x < 530)) {
+//                    pipes[8].rotateOnClick();
+//                }
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void render(SpriteBatch sb) {
+//        sb.begin();
+//        sb.draw(background, 0, 0, Test.WIDTH, Test.HEIGHT);
+//        for (Pipe pipe : pipes){
+//            sb.draw(pipe.getTexture(), pipe.getPosition().x, pipe.getPosition().y, pipe.getWidth(), pipe.getHeight());
+//        }
+//        sb.draw(checkCorrect.getTexture(), 800, 200, 100, 20);
+//        sb.end();
+//    }
+//
+//    @Override
+//    public void dispose() {
+//
+//    }
+//}

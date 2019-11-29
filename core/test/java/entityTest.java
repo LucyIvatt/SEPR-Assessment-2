c91b772193a3274f745150715e6f19a86ce5ccf7
BrunoDavies <bd775@york.ac.uk>
2019-11-29 18:34:51 +0000

    added test gradle to allow for JUnit

--- core/test/java/entityTest.java
+++ core/test/java/entityTest.java
@@ -1,9 +1,25 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class entityTest {
+
+    @BeforeAll
+    static void initAll() {
+    }
+
     @Test
     public void testAdd() {
         assertEquals(42, 42);
     }
+
+    @AfterAll
+    static void tearDownAll(){
+
+    }
+
 }
+
+
+

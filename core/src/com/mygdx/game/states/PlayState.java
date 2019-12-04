7a12641e2834385e83941b8a00d6149db0217ebd
BrunoDavies <bd775@york.ac.uk>
2019-12-04 14:11:29 +0000

    Fixed line formats

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -31,12 +31,15 @@ public class PlayState extends State {
         // https://github.com/libgdx/libgdx/wiki/Tile-maps possible way of making a map?
         background = new Texture("playbg.png");
         obstacle = new Entity(100, 100, new Texture("blue.jpg"), new Vector2(500, 400));
-        obstacle2 = new Entity(400, 400, new Texture("green.jpg"), new Vector2(400, 100));
+        obstacle2 = new Entity(400, 400, new Texture("green.jpg"),
+                new Vector2(400, 100));
         settings = Gdx.app.getPreferences("My Preferences");
         truck1 = new Firetruck(new Vector2(50, 550), 100, 100, 5, 2,
-                new Texture("truck.png"), null, 20, 10, 10, 10, true);
+                new Texture("truck.png"), null, 20, 10, 10, 10,
+                true);
         truck2 = new Firetruck(new Vector2(50, 100), 100, 100, 5, 2,
-                new Texture("truck.png"), null, 20, 10, 10, 10, false);
+                new Texture("truck.png"), null, 20, 10, 10, 10,
+                false);
         trucks.add(truck1);
         trucks.add(truck2);
     }
@@ -54,7 +57,8 @@ public class PlayState extends State {
         if (Gdx.input.isTouched()) {
             for (Firetruck truck : trucks) {
                 if (mousePos.x >= (truck.getPosition().x) && mousePos.x <= (truck.getPosition().x + truck.getWidth())
-                        && mousePos.y >= (truck.getPosition().y) && mousePos.y <= (truck.getPosition().y + truck.getHeight())) {
+                        && mousePos.y >= (truck.getPosition().y) && mousePos.y <= (truck.getPosition().y
+                        + truck.getHeight())) {
                     for (Firetruck clearTruck : trucks) {
                         clearTruck.setSelected(false);
                     }
@@ -79,14 +83,16 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.begin();
         sb.draw(background, 0, 0, Test.WIDTH, Test.HEIGHT);
-        sb.draw(obstacle.getTexture(), obstacle.getPosition().x, obstacle.getPosition().y, obstacle.getWidth(), obstacle.getHeight());
-        sb.draw(obstacle2.getTexture(), obstacle2.getPosition().x, obstacle2.getPosition().y, obstacle2.getWidth(), obstacle2.getHeight());
+        sb.draw(obstacle.getTexture(), obstacle.getPosition().x, obstacle.getPosition().y, obstacle.getWidth(),
+                obstacle.getHeight());
+        sb.draw(obstacle2.getTexture(), obstacle2.getPosition().x, obstacle2.getPosition().y, obstacle2.getWidth(),
+                obstacle2.getHeight());
         sb.draw(truck1.getTexture(), truck1.getPosition().x, truck1.getPosition().y, 100, 100);
         sb.draw(truck2.getTexture(), truck2.getPosition().x, truck2.getPosition().y, 100, 100);
         sb.end();
     }
-     // https://stackoverflow.com/questions/33283867/how-to-make-a-sprite-move-with-keyboard-in-javalibgdx?rq=1 source used
-    // Should really have a 'SPEED' constant instead of using the number 10 so that it can be changed easily.
+     //https://stackoverflow.com/questions/33283867/how-to-make-a-sprite-move-with-keyboard-in-javalibgdx?rq=1 source
+    //used. Should really have a 'SPEED' constant instead of using the number 10 so that it can be changed easily.
 
     public void truckMovement(Firetruck truck) {
         if(Gdx.input.isKeyPressed(Input.Keys.W)) {

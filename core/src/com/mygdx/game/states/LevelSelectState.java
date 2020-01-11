d344e050c15df541b5281d6c7cbcf2808b981215
Lucy <li571@york.ac.uk>
2020-01-11 19:06:10 +0000

    Added layout of buttons to levelselect

--- core/src/com/mygdx/game/states/LevelSelectState.java
+++ core/src/com/mygdx/game/states/LevelSelectState.java
@@ -1,13 +1,16 @@
 package com.mygdx.game.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.Kroy;
 import com.mygdx.game.misc.Button;
+import com.mygdx.game.sprites.Alien;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * An implementation of the abstract class State which controls the
@@ -27,30 +30,37 @@ public class LevelSelectState extends State{
     private Button level5;
     private Button level6;
     private Button back;
-    private ArrayList buttons = new ArrayList<Button>();
+    private ArrayList<Button> buttons = new ArrayList<Button>();
 
     protected LevelSelectState(GameStateManager gameStateManager) {
         super(gameStateManager);
         back = new Button(new Texture("backbutton2.png"), new Texture("backbutton1.png"),
                 100, 100, new Vector2(30, 960), false);
 
-        level1 = new Button(new Texture(".png"), new Texture(".png"),
-                350, 100, new Vector2(30, 960), false);
+        level1 = new Button(new Texture("PressedBlue1.png"), new Texture("NotPressedBlue1.png"),
+                350, 100, new Vector2(Kroy.WIDTH / 2 - 350 / 2 - 100 - 350, 400), false);
 
-        level2 = new Button(new Texture(".png"), new Texture(".png"),
-                350, 100, new Vector2(30, 960), false);
+        level2 = new Button(new Texture("PressedBlue2.png"), new Texture("NotPressedBlue2.png"),
+                350, 100, new Vector2(Kroy.WIDTH / 2 - 350 / 2 - 100 - 350, 200), false);
 
-        level3 = new Button(new Texture(".png"), new Texture(".png"),
-                350, 100, new Vector2(30, 960), false);
+        level3 = new Button(new Texture("PressedBlue3.png"), new Texture("NotPressedBlue3.png"),
+                350, 100, new Vector2(Kroy.WIDTH / 2 - 350 / 2, 400), false);
 
-        level4 = new Button(new Texture(".png"), new Texture(".png"),
-                350, 100, new Vector2(30, 960), false);
+        level4 = new Button(new Texture("PressedBlue4.png"), new Texture("NotPressedBlue4.png"),
+                350, 100, new Vector2(Kroy.WIDTH / 2 - 350 / 2, 200), false);
 
-        level5 = new Button(new Texture(".png"), new Texture(".png"),
-                350, 100, new Vector2(30, 960), false);
+        level5 = new Button(new Texture("PressedBlue5.png"), new Texture("NotPressedBlue5.png"),
+                350, 100, new Vector2(Kroy.WIDTH / 2 + 350 / 2 + 100, 400), false);
 
-        level6 = new Button(new Texture(".png"), new Texture(".png"),
-                350, 100, new Vector2(30, 960), false);
+        level6 = new Button(new Texture("PressedBlue6.png"), new Texture("NotPressedBlue6.png"),
+                350, 100, new Vector2(Kroy.WIDTH / 2 + 350 / 2 + 100, 200), false);
+
+        buttons.add(level1);
+        buttons.add(level2);
+        buttons.add(level3);
+        buttons.add(level4);
+        buttons.add(level5);
+        buttons.add(level6);
         background = new Texture("Menu.jpg");
     }
 
@@ -73,6 +83,9 @@ public class LevelSelectState extends State{
         spriteBatch.begin();
         spriteBatch.draw(background, 0, 0, Kroy.WIDTH, Kroy.HEIGHT);
         spriteBatch.draw(back.getTexture(), back.getPosition().x, back.getPosition().y, back.getWidth(), back.getHeight());
+        for (Button level : buttons) {
+            spriteBatch.draw(level.getTexture(), level.getPosition().x, level.getPosition().y, level.getWidth(), level.getHeight());
+        }
         spriteBatch.end();
     }
 

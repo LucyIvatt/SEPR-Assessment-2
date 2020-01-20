eba883fd0a82ac8d182065e728eed844fdabe420
BrunoDavies <bd775@york.ac.uk>
2020-01-20 00:11:18 +0000

    Fixed fortress

--- core/test/java/FortressTest.java
+++ core/test/java/FortressTest.java
@@ -14,12 +14,28 @@ public class FortressTest {
 
     //testing to ensure the constructor worked through testing of getters
     @Test
-    public void getSpawnRateShouldReturnGivenValue() {
+    public void constructorShouldGiveCorrectSpawnLevel1() {
         assertEquals(5.0f, testFortressLevel1.getSpawnRate());
     }
 
+    //Instance of Fortress to ensure the constructor works as intended with level 2
+    Fortress testFortressLevel2 = new Fortress(new Vector2(100, 100), 100, 100, null,
+            100, 10.0f, 2);
+
     //Fortress testFortressLevel2
+    @Test
+    public void constructorShouldGiveCorrectSpawnLevel2() {
+        assertEquals(10.0f, testFortressLevel2.getSpawnRate());
+    }
+
+    //Instance of Fortress to ensure the constructor works as intended with level 3
+    Fortress testFortressLevel3 = new Fortress(new Vector2(100, 100), 100, 100, null,
+            100, 15.0f, 3);
 
     //Fortress testFortressLevel3
+    @Test
+    public void constructorShouldGiveCorrectSpawnLevel3() {
+        assertEquals(15.0f, testFortressLevel3.getSpawnRate());
+    }
 
 }
